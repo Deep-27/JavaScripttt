16fa17b6151ad7391772bf6023a705d209bb12f0
Deep-27 <pranjal2003deep@gmail.com>
2023-06-17 04:37:44 +0530

    commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const country = "India";
+const contininet = "Asia";
+const population = "1.3 billion";
+
+console.log(country);
+console.log(contininet);
+console.log(population);
+
+let javaScript = true ;
+console.log(javaScript);
+
+
+console.log(typeof true);
+console.log(typeof country);
+console.log(typeof 69);
+
+let isIsland = false;
+let language = "Hindi";
+const finLand = " 6 million";
+
+console.log(typeof isIsland );
+console.log(country);
+console.log(typeof population );
+console.log(typeof language );
+
+console.log(population /2);
+console.log(population+1);
+console.log(population > finLand);
+console.log(population>33);
+ const description = country + " is in " + contininet + " and its " + population + " people speak " + language;
+
+ console.log(description);
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark/(heightMark*heightMark);
+const BMIJohn = massJohn/(heightJohn*heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn , markHigherBMI);
